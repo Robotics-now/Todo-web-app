fbc87279c1b33462bce17150f4c3ffab76cc83f8
Robotics-now <adityaraj.imac@gmail.com>
2025-12-20 14:40:24 -0500

    final change

--- pages/About.py
+++ pages/About.py
@@ -11,13 +11,13 @@ st.title("About This App")
 with st.expander("How to use"):
     st.write(data['about'])
 
-with st.expander("Create a ccount/login"):
+with st.expander("Create a account/login"):
     st.write(data['login_signup'])
 
 with st.expander("Check your username"):
     st.write(data['username_exitesnce'])
 
-    user_input = st.text_input(label=data['enter_username'], placeholder="example-username", key='newtodo')
+    user_input = st.text_input(label=data['enter_username'], placeholder="example-username", key='newtodo').strip().lower()
     if st.button("Check"):
         if checker(user_input.strip().lower()):
             st.warning(data['s'])
